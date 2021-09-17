fe7b202ef2bde1162c23a20df9dfeeb1f7c41e24
CyberGamer1539 <62525129+CyberGamer1539@users.noreply.github.com>
2021-09-16 21:35:33 -0600

    Add initial files

---
+++ cmake-build-debug/CMakeFiles/project_1.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Cyber/Documents/C S 236/project_1/ColonAutomaton.cpp" "C:/Users/Cyber/Documents/C S 236/project_1/cmake-build-debug/CMakeFiles/project_1.dir/ColonAutomaton.cpp.obj"
+  "C:/Users/Cyber/Documents/C S 236/project_1/ColonDashAutomaton.cpp" "C:/Users/Cyber/Documents/C S 236/project_1/cmake-build-debug/CMakeFiles/project_1.dir/ColonDashAutomaton.cpp.obj"
+  "C:/Users/Cyber/Documents/C S 236/project_1/IdentifierAutomaton.cpp" "C:/Users/Cyber/Documents/C S 236/project_1/cmake-build-debug/CMakeFiles/project_1.dir/IdentifierAutomaton.cpp.obj"
+  "C:/Users/Cyber/Documents/C S 236/project_1/Lexer.cpp" "C:/Users/Cyber/Documents/C S 236/project_1/cmake-build-debug/CMakeFiles/project_1.dir/Lexer.cpp.obj"
+  "C:/Users/Cyber/Documents/C S 236/project_1/MLCommentAutomaton.cpp" "C:/Users/Cyber/Documents/C S 236/project_1/cmake-build-debug/CMakeFiles/project_1.dir/MLCommentAutomaton.cpp.obj"
+  "C:/Users/Cyber/Documents/C S 236/project_1/SLCommentAutomaton.cpp" "C:/Users/Cyber/Documents/C S 236/project_1/cmake-build-debug/CMakeFiles/project_1.dir/SLCommentAutomaton.cpp.obj"
+  "C:/Users/Cyber/Documents/C S 236/project_1/StringAutomaton.cpp" "C:/Users/Cyber/Documents/C S 236/project_1/cmake-build-debug/CMakeFiles/project_1.dir/StringAutomaton.cpp.obj"
+  "C:/Users/Cyber/Documents/C S 236/project_1/TemplateAutomaton.cpp" "C:/Users/Cyber/Documents/C S 236/project_1/cmake-build-debug/CMakeFiles/project_1.dir/TemplateAutomaton.cpp.obj"
+  "C:/Users/Cyber/Documents/C S 236/project_1/Token.cpp" "C:/Users/Cyber/Documents/C S 236/project_1/cmake-build-debug/CMakeFiles/project_1.dir/Token.cpp.obj"
+  "C:/Users/Cyber/Documents/C S 236/project_1/main.cpp" "C:/Users/Cyber/Documents/C S 236/project_1/cmake-build-debug/CMakeFiles/project_1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/project_1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_1.dir/ColonAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/ColonDashAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/IdentifierAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/Lexer.cpp.obj"
+  "CMakeFiles/project_1.dir/MLCommentAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/SLCommentAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/StringAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/TemplateAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/Token.cpp.obj"
+  "CMakeFiles/project_1.dir/main.cpp.obj"
+  "libproject_1.dll.a"
+  "project_1.exe"
+  "project_1.exe.manifest"
+  "project_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
